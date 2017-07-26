83baa56e2af846ca4b7ad7a06a1bf86394223f55
isfaque <isfaque25@gmail.com>
2017-07-26 14:56:21 +0530

    First commit

---
+++ app/src/main/java/apps/lnsel/com/contactappdrawer/ContactFragment.java
@@ -0,0 +1,34 @@
+package apps.lnsel.com.contactappdrawer;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.InflateException;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by apps2 on 7/17/2017.
+ */
+public class ContactFragment extends Fragment {
+
+    private static View rootView;
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        if (rootView != null) {
+            ViewGroup parent = (ViewGroup) rootView.getParent();
+            if (parent != null)
+                parent.removeView(rootView);
+        }
+        try {
+            rootView = inflater.inflate(R.layout.fragment_contact, container, false);
+        } catch (InflateException e) {
+
+        }
+
+        return rootView;
+    }
+}
